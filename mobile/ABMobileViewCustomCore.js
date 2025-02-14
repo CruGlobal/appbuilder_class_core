4f310cffb6568668e0719896fad7f39f5946a08b
Johnny <johnny@digiserve.org>
2025-02-14 16:57:46 -0600

    [add] include the custom view components

---
+++ mobile/ABMobileViewCustomCore.js
@@ -0,0 +1,58 @@
+const ABMobileView = require("../../platform/mobile/ABMobileView");
+
+const ABViewCustomPropertyComponentDefaults = {
+   dataviewID: null,
+   field: null,
+   height: 0,
+   hideTitle: 0,
+   hideTabs: 0,
+};
+
+const ABViewDefaults = {
+   key: "mobile-custom", // {string} unique key for this view
+   icon: "palette", // {string} fa-[icon] reference for this view
+   labelKey: "Custom", // {string} the multilingual label key for the class label
+};
+
+module.exports = class ABViewCustomCore extends ABMobileView {
+   constructor(values, application, parent, defaultValues) {
+      super(values, application, parent, defaultValues || ABViewDefaults);
+   }
+
+   static common() {
+      return ABViewDefaults;
+   }
+
+   static defaultValues() {
+      return ABViewCustomPropertyComponentDefaults;
+   }
+
+   /**
+    * @method componentList
+    * return the list of components available on this view to display in the editor.
+    */
+   componentList() {
+      return [];
+   }
+
+   // field() {
+   //    var dv = this.datacollection;
+   //    if (!dv) return null;
+
+   //    var object = dv.datasource;
+   //    if (!object) return null;
+
+   //    return object.fieldByID(this.settings.field);
+   // }
+
+   /**
+    * @method wantsAdd()
+    * Some widgets can indicate to their containing ABMobilePage that
+    * it wants to provide an [Add] feature.
+    * @return {bool}
+    */
+   get wantsAdd() {
+      // we do if we have a setting for linkPageAdd
+      return this.settings.wantsAdd ?? false;
+   }
+};
