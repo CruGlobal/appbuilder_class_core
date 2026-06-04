34ea6f8ca48d26610feeaff8fd1de00c3598a7c9
BenWirachot <ben.thongtiang@cru.org>
2026-06-04 13:25:15 +0700

    Update ABFieldTreeCore compatible types to json

--- dataFields/ABFieldTreeCore.js
+++ dataFields/ABFieldTreeCore.js
@@ -55,12 +55,12 @@ const ABFieldTreeDefaults = {
    // determines if this ABField can be used in the display of an ABObject's
    // label.
 
-   compatibleOrmTypes: ["string"],
+   compatibleOrmTypes: ["json"],
    // {array}
    // what types of Sails ORM attributes can be imported into this data type?
    // http://sailsjs.org/documentation/concepts/models-and-orm/attributes#?attribute-options
 
-   compatibleMysqlTypes: ["text", "mediumtext", "longtext"],
+   compatibleMysqlTypes: ["json"],
    // {array}
    // what types of MySql column types can be imported into this data type?
    // https://www.techonthenet.com/mysql/datatypes.php
